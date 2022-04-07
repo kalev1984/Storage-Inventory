fffc516ba55da34b446e95fea02f1b3116ef13ea
Kalev <Kalev@>
2022-04-07 18:08:19 +0300

    minor fix

--- Storage Inventory/WebApp/ApiControllers/StatisticsController.cs
+++ Storage Inventory/WebApp/ApiControllers/StatisticsController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using App.Contracts.DAL;
 using App.Domain.Identity;
 using App.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ namespace WebApp.ApiControllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize(Roles = "admin")]
 public class StatisticsController : Controller
 {
     private readonly IAppUnitOfWork _uow;
